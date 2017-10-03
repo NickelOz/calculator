3c485e5240c73336385601570bb2d0a14487740f
Nicholas Whittaker <NickelOz@users.noreply.github.com>
2017-10-03 12:01:41 +1100

    Added button press functionality

--- src/index.js
+++ src/index.js
@@ -1,29 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Calculator extends React.Component {
-    render() {
-        return (
-            <div id="calculator">
-                {this.renderButton()}
-            </div>
-        )
-    }
-
-    renderButton() {
-        return <Button value={2} />
-    }
-}
 
 class Button extends React.Component {
     render() {
         return (
-            <div className="button" onClick={() => alert('clicked')}>
+            <div className="button" onClick={() => this.props.onClick()}>
                 {this.props.value}
             </div>
         )
     }
 }
+
+class Calculator extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            currentTerm: 0,
+            currentOperator: null,
+            previousTerm: null
+        }
+    }
+
+    renderNumberButton(i) {
+        return <Button value={i} onClick={() => this.addNumber(i)}/>
+    }
+
+    addNumber(i) {
+        this.setState({currentTerm: i}, () => {alert(`The current term is ${this.state.currentTerm}`)})
+    }
+
+    render() {
+        return (
+            <div id="calculator">
+                {this.renderNumberButton(1)}
+                {this.renderNumberButton(2)}
+                {this.renderNumberButton(3)}
+            </div>
+        )
+    }
+}
+
 
 ReactDOM.render(
     <Calculator />,
